c0640e6b11d1cc9792df7310b1fa6762d982be19
Fredrik <fredrik.m.borjesson@gmail.com>
2019-10-23 20:13:05 +0200

    Final changes again

--- app/src/main/java/com/example/fredify/ui/main/CountrySelectFragment.kt
+++ app/src/main/java/com/example/fredify/ui/main/CountrySelectFragment.kt
@@ -1,21 +1,13 @@
 package com.example.fredify.ui.main
 
 import android.os.Bundle
-import android.text.Html
-import android.text.method.LinkMovementMethod
-import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
-import android.view.View.OnClickListener
 import android.view.ViewGroup
-import android.widget.AdapterView
 import android.widget.ArrayAdapter
-import androidx.appcompat.widget.AppCompatButton
-import androidx.core.text.HtmlCompat
+import androidx.fragment.app.Fragment
 import com.example.fredify.R
-import kotlinx.android.synthetic.main.language_selection_layout.*
-import kotlinx.android.synthetic.main.login_fragment.*
+import kotlinx.android.synthetic.main.country_selection_fragment.*
 
 class CountrySelectFragment : Fragment() {
 
@@ -27,12 +19,8 @@ class CountrySelectFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View {
-        val contentView = inflater.inflate(R.layout.language_selection_layout, container, false)
+        val contentView = inflater.inflate(R.layout.country_selection_fragment, container, false)
         return contentView
-    }
-
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -50,6 +38,9 @@ class CountrySelectFragment : Fragment() {
 
     private fun buttonClicked() : Unit {
         val fragment = LoginFragment.newInstance()
-        activity!!.supportFragmentManager.beginTransaction().replace(R.id.container, fragment).commit()
+        val transaction = activity!!.supportFragmentManager.beginTransaction()
+        transaction.setCustomAnimations(R.anim.enter_animation, R.anim.exit_animation)
+        transaction.replace(R.id.container, fragment)
+        transaction.commit()
     }
 }
